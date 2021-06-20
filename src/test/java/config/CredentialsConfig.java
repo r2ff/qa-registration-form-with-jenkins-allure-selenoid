e36a43328cf344e37d7322baa598672e2d8f1e54
Alexey <reni.gate@gmail.com>
2021-06-20 20:39:30 +0500

    added owner #3

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/credentials.properties"
+        "classpath:config/Credentials.properties"
 })
 
 public interface CredentialsConfig extends Config {
